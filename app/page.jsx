3e402ee733ace5f29902e8dcb10389a9e6b36c54
JoongBuGit <joongbu_re25@jmail.ac.kr>
2023-12-11 17:18:01 +0900

    10 commit

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,8 @@ export default function Home() {
   return (
     <div>
       <h1 className="text_header">홈화면입니다!</h1>
+
+      <img width={700} src="/img/home.png"></img>
       <ul className="text_p">
         포트폴리오 프로젝트들을 소개하는 홈페이지입니다~
       </ul>
